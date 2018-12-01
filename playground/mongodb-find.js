690ead36fff3901080484f464e11016c41fd25a9
Satoshi Kawase <kawase@jhu.edu>
2018-12-01 22:49:41 +0900

    first commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // //.find() returns pointer only
+  // db.collection('Todos').find({
+  //   _id:new ObjectID('5c028d9e86c93b802ee9c780')
+  // }).toArray().then(function(docs){
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, function(err){
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+  //.find() returns pointer only
+  // db.collection('Todos').find().count().then(function(count){
+  //   console.log(`Todos count: ${count}`);
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, function(err){
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+  db.collection('Users').find({name:'Toshi'}).toArray().then(function(docs){
+    console.log('Toshi users');
+    console.log(JSON.stringify(docs, undefined,2));
+  }, function(err){
+    console.log('Unable to fetch users');
+  });
+
+
+
+
+  // client.close();
+});
