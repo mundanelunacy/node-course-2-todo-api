5e83f407a956416f3638c3c177588927e0da1964
Satoshi Kawase <kawase@jhu.edu>
2018-12-03 20:28:56 +0900

    added mongoose-queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,49 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose.js');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = "5c04b75531b5e832233d02ed11";
+//
+// if(!ObjectID.isValid(id)){
+//   console.log('ID not valid');
+// }
+//
+// Todo.find({_id: id}).then(function(todos){
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({_id: id}).then(function(todo){
+//   console.log('Todo', todo);
+// });
+//
+// Todo.findById(id).then(function(todo){
+//   if(!todo){
+//     return console.log('Id not found');
+//   }
+//   console.log('TodoBy Id', todo);
+// }).catch((e) => console.log(e));
+
+
+// User.findById
+// user not found
+// print user to screen
+// handle errors
+
+
+var userId = '5c0369509a749cffdb4786fe';
+if(!ObjectID.isValid(userId)){
+  console.log('ID is not valid');
+}else{
+  User.findById(userId).then(function(user){
+    if(user){
+      console.log(user);
+    }else {
+      console.log('unable to find user');
+    }
+
+  }, function(e){
+    console.log(e);
+  }).catch((e) => console.log(e));
+}
