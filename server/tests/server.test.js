efffdd5dbcff95b11ad8a8fce1828f8943b14a9b
Satoshi Kawase <kawase@jhu.edu>
2018-12-03 13:56:09 +0900

    add test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+},{
+  text: 'Second test todo'
+}];
+
+
 beforeEach(function(done){
   Todo.remove({}).then(function(){
-    done();
-  });
+    return Todo.insertMany(todos);
+  }).then(()=>done());
 });
 
 describe('POST /todos', function(){
@@ -26,7 +33,7 @@ describe('POST /todos', function(){
         if(err){
           return done(err);
         }
-        Todo.find().then(function(todos){
+        Todo.find({text}).then(function(todos){
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -46,10 +53,23 @@ describe('POST /todos', function(){
           return done(e);
         }
         Todo.find().then(function(todos){
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
 
   });
-})
+});
+
+
+describe('GET /todos', function(){
+  it('should get all todos', function(done){
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(function(res){
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
+});
