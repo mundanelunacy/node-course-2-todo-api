0c3476e5077707ffaad241b39b7a4f17d0544fbd
Satoshi Kawase <kawase@jhu.edu>
2018-12-08 22:39:38 +0900

    added PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+
 var {mongoose} = require ('./db/mongoose');
 var {Todo} = require ('./models/todo');
 var {User} = require ('./models/user');
@@ -74,6 +75,34 @@ app.delete('/todos/:id', function (req, res){
   });
 });
 
+app.patch('/todos/:id', function(req, res){
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set : body}, {new: true}).then(function(todo){
+    if(!todo){
+      res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }).catch(function(e){
+    res.status(400).send();
+  });
+
+
+})
 
 app.listen(port, function(){
   console.log(`Started on port ${port}`);
